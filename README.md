27047a76951d9fc7514a1e5dc6cc7fe8ab4264d6
Thomas Lee <tjl352@macpro.myfiosgateway.com>
2019-12-12 02:06:58 -0500

    WIP: Inbetween page objects with module exports or es6

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # react-compare-app-e2e
+
+steps taken for setup
+mkdir react-compare-app-e2e && cd react-compare-app-e2e
+npm init -y
+npm i --save-dev @wdio/cli
+./node_modules/.bin/wdio config -y
+mkdir -p ./test/specs
+touch ./test/specs/basic.js
+./node_modules/.bin/wdio wdio.conf.js
+
+to run
+
+git clone https://github.com/tjl352/react-compare-app-e2e.git
+
+npm install
+
+./node_modules/.bin/wdio wdio.conf.js
---
+++ test/specs/home.spec.js
@@ -0,0 +1,137 @@
+const assert = require('assert');
+const homePage = require('../pages/home.page');
+
+describe('react-compare-app page', () => {
+  //const appFruits = [];
+
+  beforeEach(() => {
+    // arrFruits = [
+    //   $('//*[@id="root"]/div/div/div/div[2]/div[1]/div/div[2]'),
+    //   $('//*[@id="root"]/div/div/div/div[2]/div[2]/div/div[2]'),
+    //   $('//*[@id="root"]/div/div/div/div[2]/div[3]/div/div[2]'),
+    //   $('//*[@id="root"]/div/div/div/div[2]/div[4]/div/div[2]')
+    // ];
+    homePage.open();
+  });
+
+  it('should have the right title', () => {
+    assert.strictEqual(homePage.getPageTitle(), 'React App');
+  });
+
+  it('should display table when all 4 items selected', () => {
+    //click all
+    // for (let i = 0; i < arrFruits.length; i++) {
+    //   arrFruits[i].click();
+    // }
+    homePage.clickAllFruits();
+    assert($('div.row.compare').isDisplayed());
+
+    //assert table header names
+    assert.equal($('//tr/th[2]').getText(), 'Cherry');
+    assert.equal($('//tr/th[3]').getText(), 'Orange');
+    assert.equal($('//tr/th[4]').getText(), 'Nuts');
+    assert.equal($('//tr/th[5]').getText(), 'Strawberry');
+
+    //assert all prices
+    const actualPrices = $$('td.text-center');
+    const expectedPrices = ['$1.99', '$2.99', '$1.00', '$1.49'];
+
+    for (let i = 0; i < actualPrices.length; i++) {
+      assert.equal(actualPrices[i].getText(), expectedPrices[i]);
+    }
+
+    //assert all colors
+    const expectedColors = [
+      'bg-red',
+      'bg-green',
+      'bg-blue',
+      'bg-green',
+      'bg-blue',
+      'bg-red',
+      'bg-blue'
+    ];
+    const actualColors = $$('tr.colors td span');
+    for (let i = 0; i < expectedColors.length; i++) {
+      assert.equal(actualColors[i].getAttribute('class'), expectedColors[i]);
+    }
+
+    //assert all conditions
+    const expectedConditions = ['Fresh', 'Frozen', 'Frozen', 'Fresh'];
+    const actualConditions = $$('tr.condition td');
+    for (let i = 0; i < expectedConditions.length; i++) {
+      assert.equal(actualConditions[i].getText(), expectedConditions[i]);
+    }
+  });
+
+  // it('should display table when any 3 items selected', () => {
+  //   browser.url('https://react-compare-app.surge.sh/');
+
+  //   //all but cherry
+  //   arrFruits[1].click();
+  //   arrFruits[2].click();
+  //   arrFruits[3].click();
+  //   assert($('div.row.compare').isDisplayed());
+  //   browser.refresh();
+
+  //   //all but orange
+  //   arrFruits[0].click();
+  //   arrFruits[2].click();
+  //   arrFruits[3].click();
+  //   assert($('div.row.compare').isDisplayed());
+  //   browser.refresh();
+
+  //   //all but nuts
+  //   arrFruits[0].click();
+  //   arrFruits[1].click();
+  //   arrFruits[3].click();
+  //   assert($('div.row.compare').isDisplayed());
+  //   browser.refresh();
+
+  //   //all but strawberry
+  //   arrFruits[0].click();
+  //   arrFruits[1].click();
+  //   arrFruits[2].click();
+  //   assert($('div.row.compare').isDisplayed());
+  //   browser.refresh();
+  // });
+
+  // it('should display table when any 2 items selected', () => {
+  //   browser.url('https://react-compare-app.surge.sh/');
+
+  //   //cherry/orange
+  //   arrFruits[0].click();
+  //   arrFruits[1].click();
+  //   assert($('div.row.compare').isDisplayed(), 'cherry/orange');
+  //   browser.refresh();
+
+  //   //cherry/nuts
+  //   arrFruits[0].click();
+  //   arrFruits[2].click();
+  //   assert($('div.row.compare').isDisplayed(), 'cherry/nuts');
+  //   browser.refresh();
+
+  //   //cherry/strawberry
+  //   arrFruits[0].click();
+  //   arrFruits[3].click();
+  //   assert($('div.row.compare').isDisplayed(), 'cherry/strawberry');
+  //   browser.refresh();
+
+  //   //orange/nuts
+  //   arrFruits[1].click();
+  //   arrFruits[2].click();
+  //   assert($('div.row.compare').isDisplayed(), 'orange/nuts');
+  //   browser.refresh();
+
+  //   //orange/strawberry
+  //   arrFruits[1].click();
+  //   arrFruits[3].click();
+  //   assert($('div.row.compare').isDisplayed(), 'orange/stawberry');
+  //   browser.refresh();
+
+  //   //nuts/strawberry
+  //   arrFruits[2].click();
+  //   arrFruits[3].click();
+  //   assert($('div.row.compare').isDisplayed(), 'nuts/strawberry');
+  //   browser.refresh();
+  // });
+});
